b6a6413510c44ad8e1de15caf06a202559ab9a6a
wowbob396 <wowbob396@gmail.com>
2018-01-28 03:18:36 -0600

    created sqlite tables

--- backend/Conkdata/models.py
+++ backend/Conkdata/models.py
@@ -7,9 +7,11 @@ class User(models.Model):
     weight = models.IntegerField()
     height = models.IntegerField()
     user_id = models.IntegerField()
+    bucket_name = models.CharField(max_length=200)
 
 class Injury(models.Model):
     user_id = models.IntegerField()
     injury_type = models.CharField(max_length=200)
-    symptoms = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    symptoms = models.CharField(max_length=400)
+    bench_date = models.DateTimeField('date benched')
+    unbench_date = models.DateTimeField('date unbenched')
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'Conkdata.apps.ConkdataConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
